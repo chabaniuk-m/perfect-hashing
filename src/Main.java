6c5fea283f9d4df829507770b15827179223fa66
chabanyukm <chabanyuk.maks@gmail.com>
2022-02-18 09:01:27 +0200

    User input - from file (test 7)

--- src/Main.java
+++ src/Main.java
@@ -1,9 +1,11 @@
 import hashing.hash.Perfect;
 import hashing.hash.Universal;
 import hashing.Util;
-import hashing.key.Complex;
 import hashing.key.Vector;
 
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Paths;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -15,21 +17,27 @@ public class Main {
         System.out.println("\t\t\t\t\t\t\t***** PERFECT HASH FUNCTION ********");
         System.out.println("\t\t\t\t\t\t\t************************************\n");
 
-        runTestNumber(3);
+        var keys = runTestNumber(7);
+
+        showKeys(keys);
+        var h = new Perfect();
+        var table = h.buildFor(keys);
+
+        System.out.println(table);
     }
 
-    private static void runTestNumber(int number) {
+    private static List<Vector> runTestNumber(int number) {
         System.out.printf("[ℹ] Test number %d\n", number);
-        switch (number) {
-            case 1 -> test1();
-            case 2 -> test2();
-            case 3 -> test3();
-            case 4 -> test4();
-            case 5 -> test5();
-            case 6 -> test6();
+        return switch (number) {
+            case 1 -> Util.randData(5, 1);
+            case 2 -> Util.randData(20, 1);
+            case 3 -> Util.randData(1000, 1);
+            case 4 -> Util.randData(5);
+            case 5 -> Util.randData(20);
+            case 6 -> Util.randData(1000);
             case 7 -> test7();
-            default -> System.out.println("KILL Me plz");
-        }
+            default -> null;
+        };
     }
 
     private static void showKeys(List<Vector> keys) {
@@ -45,69 +53,19 @@ public class Main {
         System.out.println("\n         }\n");
     }
 
-    private static void test1() {
-
-        var keys = Util.randData(5, 1);
-        showKeys(keys);
-        var h = new Perfect();
-        var table = h.buildFor(keys);
-
-        System.out.println(table);
-    }
-
-    private static void test2() {
-
-        var keys = Util.randData(20, 1);
-        showKeys(keys);
-        var h = new Perfect();
-        var table = h.buildFor(keys);
-
-        System.out.println(table);
-    }
-
-    private static void test3() {
-
-        var keys = Util.randData(1000, 1);
-        showKeys(keys);
-        var h = new Perfect();
-        var table = h.buildFor(keys);
-
-        System.out.println(table);
-    }
-
-    private static void test4() {
-
-        var keys = Util.randData(5);
-        showKeys(keys);
-        var h = new Perfect();
-        var table = h.buildFor(keys);
-
-        System.out.println(table);
-    }
-
-    private static void test5() {
-
-        var keys = Util.randData(20);
-        showKeys(keys);
-        var h = new Perfect();
-        var table = h.buildFor(keys);
-
-        System.out.println(table);
-    }
-
-    private static void test6() {
-
-        var keys = Util.randData(1000);
-        showKeys(keys);
-        var h = new Perfect();
-        var table = h.buildFor(keys);
-
-        System.out.println(table);
-    }
-
     // TODO: 2/14/2022 test from file
-    private static void test7() {
-
+    private static List<Vector> test7() {
+        List<Vector> keys = null;
+        try {
+            var lines = Files.readAllLines(Paths.get("file.txt"));
+            keys = new ArrayList<>(lines.size());
+            for (var line : lines) {
+                keys.add(new Vector(List.of(Util.parseComplex(line))));
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        return keys;
     }
 
     private static void testVectors(int n, int dimension) {
